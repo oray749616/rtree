d1003f08126d036742f4b21172d4d8e643a53d6d
oray749616 <oray749616@gmail.com>
2021-12-28 01:52:17 +0800

    feat: 实现 Point 方法

--- src/com/weilai/rtree/Point.java
+++ src/com/weilai/rtree/Point.java
@@ -1,8 +1,100 @@
 package com.weilai.rtree;
 
+import java.util.Arrays;
+
 /**
  * @ClassName Point
- * @Description: TODO
+ * @Description: n维空间中的点，所有的维度被存储在一个float数组中
  */
 public class Point {
+	private float[] data;
+	
+	public Point(float[] data) {
+		if (data == null) {
+			throw new IllegalArgumentException("Coordinates can't be empty.");
+		}
+		if (data.length < 2) {
+			throw new IllegalArgumentException("The dimension of the point must be greater than 1.");
+		}
+		
+		this.data = new float[data.length];
+		System.arraycopy(data, 0, this.data, 0, data.length);
+	}
+
+	public Point(int[] data) {
+		if (data == null) {
+			throw new IllegalArgumentException("Coordinates can't be empty.");
+		}
+		if (data.length < 2) {
+			throw new IllegalArgumentException("The dimension of the point must be greater than 1.");
+		}
+
+		this.data = new float[data.length];
+		for (int i = 0; i < data.length; i++) {
+			this.data[i] = data[i];
+		}
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		float[] copy = new float[data.length];
+		System.arraycopy(data, 0, copy, 0, data.length);
+		return new Point(copy);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("(");
+
+		for (int i = 0; i < data.length - 1; i++) {
+			sb.append(data[i]).append(",");
+		}
+		sb.append(data[data.length - 1]).append(")");
+
+		return sb.toString();
+	}
+
+	/**
+	 * @return 返回 Point 的维度
+	 */
+	public int getDimension() {
+		return data.length;
+	}
+
+	/**
+	 * @param index 下标
+	 * @return 返回 Point 坐标的第 i 位的 int 值
+	 */
+	public float getFloatCoordinate(int index) {
+		return data[index];
+	}
+
+	/**
+	 * @param index 下标
+	 * @return 返回 Point 坐标的第 i 位的 int 值
+	 */
+	public int getIntCoordinate(int index) {
+		return (int) data[index];
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// obj 是 Point 的实例
+		if (obj instanceof Point) {
+			Point point = (Point) obj;
+
+			// 维度相同的点才能比较
+			if (point.getDimension() != getDimension()) {
+				throw new IllegalArgumentException("Only points with the same dimension can be compared.");
+			}
+
+			for (int i = 0; i < getDimension(); i++) {
+				if (getFloatCoordinate(i) != point.getFloatCoordinate(i)) {
+					return false;
+				}
+			}
+		}
+
+		return obj instanceof Point;
+	}
 }
